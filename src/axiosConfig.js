d03acd1cc656da042711a384f9e955fb09e7e779
KwaminaOtabil <stanleykwaminaotabil@gmail.com>
2024-10-03 21:08:04 +0000

    a

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://backend.baccheck.online/api/",
+  baseURL: "https://backend.baccheck.online/api",
   headers: {
     "Content-Type": "multipart/form-data",
+    Accept: "application/json",
   },
 });
 
@@ -12,7 +13,7 @@ instance.interceptors.request.use(
     const authToken = localStorage.getItem("authToken");
 
     if (authToken) {
-      config.headers["Authorization"] = "Token " + authToken;
+      config.headers["Authorization"] = "Bearer " + authToken;
     }
     return config;
   },
