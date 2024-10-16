41530884f66a5794134fb98697e199d243ddabf3
KwaminaOtabil <stanleykwaminaotabil@gmail.com>
2024-10-16 23:03:27 +0000

    login refactor

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -4,7 +4,9 @@ import withRouter from "../../components/withRouter";
 import ReCAPTCHA from "react-google-recaptcha";
 import { toast } from "react-hot-toast";
 import Spinner from "../../components/Spinner";
-import { LuEye, LuEyeOff } from "react-icons/lu";
+import { LuEye, LuEyeOff, LuUser } from "react-icons/lu";
+import { Button, Card, Input } from "@nextui-org/react";
+import ThemeSwitcher from "../../components/ThemeSwitcher";
 
 class Login extends Component {
   constructor(props) {
@@ -58,11 +60,11 @@ class Login extends Component {
       this.setState({ isLoading: false });
 
       const type = localStorage.getItem("type");
-      
-      if(responseData.institution.status == 'inactive'){
-        this.props.navigate('/account-inactive')
-        
-        return
+
+      if (responseData.institution.status == "inactive") {
+        this.props.navigate("/account-inactive");
+
+        return;
       }
 
       if (type == "institution") {
@@ -98,122 +100,96 @@ class Login extends Component {
     } = this.state;
     return (
       <>
-        <div className="h-screen w-full flex justify-center items-center md:px-4 lg:px-0 bg-white md:bg-gray-100">
-          <div className="md:w-2/5 lg:w-1/3 2xl:w-1/4 w-full pb-10 md:px-4 lg:px-6 bg-white md:shadow-xl">
-            <form className="mt-6">
+        <div className="h-screen w-full flex flex-col justify-center items-center md:px-4 lg:px-0 dark:bg-slate-950">
+          <ThemeSwitcher />
+          <Card className="md:w-2/5 lg:w-1/3 2xl:w-1/4 w-full pb-10 md:px-4 lg:px-6 dark:bg-slate-900 md:shadow-xl">
+            <form className="mt-6 flex flex-col gap-5">
               <div className="text-center text-yellow-100">
                 <div className="flex items-center justify-center mx-auto w-32 h-32">
                   <img src="/images/bclogo.jpg" alt="BacChecker Logo" />
                 </div>
               </div>
               <div className="flex flex-col text-center mt-4">
-                <p className="font-bold text-gray-700 text-base">Login to your account</p>
-                <p className="text-gray-700 text-xs">Access your Institution account by logging in</p>
+                <p className="font-bold text-gray-700 dark:text-white text-base">
+                  Login to your account
+                </p>
+                <p className="text-gray-700 dark:text-white text-xs">
+                  Access your Institution account by logging in
+                </p>
               </div>
-              <div className="relative my-6 mx-5 md:mx-0">
-                <input
-                  type="text"
-                  name="email"
-                  value={email}
-                  onChange={(e) => this.setState({ email: e.target.value })}
-                  className="block rounded-md px-2.5 pb-1 pt-5 pl-10 w-full text-sm text-gray-900 bg-blue-50 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                />
+              <Input
+                label="Email"
+                type="email"
+                name="email"
+                value={email}
+                className="mt-1 block w-full"
+                autoComplete="email"
+                autoFocus
+                onChange={(e) => this.setState({ email: e.target.value })}
+                startContent={<LuUser />}
+                isInvalid={emailError}
+                errorMessage={emailError}
+              />
 
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-person w-5 h-5"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-                  </svg>
-                </div>
-                <div className="text-xs text-red-600 italic">{emailError}</div>
-                <label
-                  htmlFor="floating_filled"
-                  className="absolute text-base text-gray-500 duration-300 pl-10 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-gray-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
-                >
-                  Email
-                </label>
-              </div>
-              <div className="relative my-6 mx-5 md:mx-0">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  id="password"
-                  name="password"
-                  value={password}
-                  onChange={(e) => this.setState({ password: e.target.value })}
-                  className="block rounded-md pr-6 pb-1 pt-5 pl-10 w-full text-sm text-gray-900 bg-blue-50 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                />
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    className="bi bi-lock w-5 h-5"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1" />
-                  </svg>
-                </div>
-                <div className="flex absolute inset-y-0 items-center right-0 pr-4">
-                  {!showPassword ? (
-                    <LuEye size={24} onClick={this.handleClickShowPassword} className="cursor-pointer text-gray-700"/>
+              <Input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                value={password}
+                label="Email"
+                className="mt-1 block w-full"
+                autoComplete="email"
+                autoFocus
+                onChange={(e) => this.setState({ password: e.target.value })}
+                startContent={<LuUser />}
+                isInvalid={emailError}
+                errorMessage={emailError}
+                endContent={
+                  !showPassword ? (
+                    <LuEye
+                      size={24}
+                      onClick={this.handleClickShowPassword}
+                      className="cursor-pointer"
+                    />
                   ) : (
-                    <LuEyeOff size={24} onClick={this.handleClickShowPassword} className="cursor-pointer text-gray-700"/>
-                  )}
-                </div>
-                <div className="text-sm text-red-600 italic">
-                  {passwordError}
-                </div>
-                <label
-                  htmlFor="floating_filled"
-                  className="absolute text-base text-gray-500 duration-300 transform -translate-y-4 pl-10 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-gray-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="px-2 pb-2">
+                    <LuEyeOff
+                      size={24}
+                      onClick={this.handleClickShowPassword}
+                      className="cursor-pointer"
+                    />
+                  )
+                }
+              />
+
+              <div className="flex ">
                 <ReCAPTCHA
                   sitekey="6LeT50QqAAAAAOjlgT3V74eIOT3DwvtemCjWOM-K"
                   onChange={this.onRecaptchaChange}
                   ref={this.recaptchaRef}
+                  className="mx-auto"
                 />
               </div>
 
-              <div className="md:px-0 px-5">
-                <button
-                  type="submit"
-                  onClick={this.loginUser}
-                  disabled={isLoading}
-                  className={`w-full flex items-center justify-center mr-2 ${
-                    isLoading
-                      ? "bg-gray-400 text-gray-700 cursor-not-allowed"
-                      : "hover:bg-buttonLog bg-bChkRed text-white"
-                  }  rounded-md py-2.5 md:mb-1 mb-5 ${
-                    isLoading ? "cursor-not-allowed" : ""
-                  }`}
-                >
-                  {isLoading ? (
-                    <>
-                      <Spinner size='w-5 h-5 mr-2'/>
-                      Logging In...
-                    </>
-                  ) : (
-                    "Log In"
-                  )}
-                </button>
-              </div>
-              <p className="text-center text-sm text-gray-600">
+              <Button
+                color="danger"
+                onClick={this.loginUser}
+                isLoading={isLoading}
+                disabled={isLoading}
+              >
+                Login
+              </Button>
+
+              <p className="text-center text-sm text-gray-600 dark:text-white">
                 Forgot your Password?{" "}
-                <a href="https://backend.baccheck.online/forgot-password" className="text-red-600">
+                <a
+                  href="https://backend.baccheck.online/forgot-password"
+                  className="text-red-600"
+                >
                   Reset
                 </a>
               </p>
             </form>
-          </div>
+          </Card>
         </div>
       </>
     );
