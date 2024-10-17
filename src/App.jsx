9e37246efe83fd6a6020fae6bc39a8eb9e1652f1
KwaminaOtabil <stanleykwaminaotabil@gmail.com>
2024-10-17 00:35:43 +0000

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
-import Dashboard from './pages/Dashboard';
-import {toast} from 'react-hot-toast';
+import React, { Component } from "react";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import { toast } from "react-hot-toast";
 import { Routes, Route } from "react-router-dom";
-import Login from './pages/auth/Login';
-import withRouter from './components/withRouter';
-import CompleteProfile from './pages/CompleteProfile';
-import DocumentRequest from './pages/document-request/DocumentRequest';
-import AddDocumentType from './pages/AddDocumentType';
-import VerifyOTP from './pages/auth/VerifyOTP';
-import Toastify from './components/Toastify';
-import DocumentTypes from './pages/DocumentTypes';
-import ValidationQuestions from './pages/ValidationQuestions';
-import Profile from './pages/user-profile/Profile';
-import InstitutionData from './pages/complete-profile/InstitutionData';
-import OperationsCert from './pages/complete-profile/OperationsCert';
-import InstitutionTeams from './pages/institution-teams/InstitutionTeams';
-import InstitutionUsers from './pages/institution-teams/InstitutionUsers';
-import InstitutionLetter from './pages/complete-profile/InstitutionLetter';
-import DocumentDetails from './pages/document-request/DocumentDetails';
-import AccountInactive from './pages/complete-profile/AccountInactive'
+import Login from "./pages/auth/Login";
+import withRouter from "./components/withRouter";
+import CompleteProfile from "./pages/CompleteProfile";
+import DocumentRequest from "./pages/document-request/DocumentRequest";
+import AddDocumentType from "./pages/AddDocumentType";
+import VerifyOTP from "./pages/auth/VerifyOTP";
+import Toastify from "./components/Toastify";
+import DocumentTypes from "./pages/DocumentTypes";
+import ValidationQuestions from "./pages/ValidationQuestions";
+import Profile from "./pages/user-profile/Profile";
+import InstitutionData from "./pages/complete-profile/InstitutionData";
+import OperationsCert from "./pages/complete-profile/OperationsCert";
+import InstitutionTeams from "./pages/institution-teams/InstitutionTeams";
+import InstitutionUsers from "./pages/institution-teams/InstitutionUsers";
+import InstitutionLetter from "./pages/complete-profile/InstitutionLetter";
+import DocumentDetails from "./pages/document-request/DocumentDetails";
+import AccountInactive from "./pages/complete-profile/AccountInactive";
 
 class App extends Component {
   state = {
@@ -37,8 +37,12 @@ class App extends Component {
   };
 
   handleClickOutside = (e) => {
-    const sidebarElement = document.querySelector('.sidebar');
-    if (sidebarElement && !sidebarElement.contains(e.target) && window.innerWidth < 768) {
+    const sidebarElement = document.querySelector(".sidebar");
+    if (
+      sidebarElement &&
+      !sidebarElement.contains(e.target) &&
+      window.innerWidth < 768
+    ) {
       this.setState({ isSidebarVisible: false });
     }
   };
@@ -48,8 +52,8 @@ class App extends Component {
       const response = await axios.get("/institution/institution-data");
       const institutionData = response.data.institutionData;
 
-      if (institutionData.status == 'inactive') {
-        this.props.navigate('/account-inactive')
+      if (institutionData.status == "inactive") {
+        this.props.navigate("/account-inactive");
       }
     } catch (error) {
       toast.error(error.response.data.message);
@@ -57,60 +61,117 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.fetchInstitution()
-    document.addEventListener('mousedown', this.handleClickOutside);
+    this.fetchInstitution();
+    document.addEventListener("mousedown", this.handleClickOutside);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener("mousedown", this.handleClickOutside);
   }
 
   render() {
     const { isSidebarVisible, isSidebarCollapsed } = this.state;
-    const isLoginPage = location.pathname === '/login' || location.pathname === '/' || location.pathname === '/complete-profile' || location.pathname === '/verify-otp';
-
+    const isLoginPage =
+      location.pathname === "/login" ||
+      location.pathname === "/" ||
+      location.pathname === "/complete-profile" ||
+      location.pathname === "/verify-otp";
 
     return (
-      <div className="flex h-screen bg-gray-100 font-figtree">
+      <div className="flex h-screen bg-gray-100 dark:bg-slate-950 font-figtree">
         <Toastify />
         {/* Sidebar */}
-        {!isLoginPage && <Sidebar
-          isVisible={isSidebarVisible}
-          isCollapsed={isSidebarCollapsed}
-          closeSidebar={this.toggleMobileSidebar}
-        />}
+        {!isLoginPage && (
+          <Sidebar
+            isVisible={isSidebarVisible}
+            isCollapsed={isSidebarCollapsed}
+            closeSidebar={this.toggleMobileSidebar}
+          />
+        )}
 
         {/* Main content area */}
         <div
-          className={`flex-1 ${!isLoginPage && isSidebarCollapsed ? 'lg:ml-16' : !isLoginPage ? 'lg:ml-[17%]' : ''} transition-margin duration-300`}
+          className={`flex-1 ${
+            !isLoginPage && isSidebarCollapsed
+              ? "lg:ml-16"
+              : !isLoginPage
+              ? "lg:ml-[17%]"
+              : ""
+          } transition-margin duration-300`}
         >
           {/* Navbar */}
-          {!isLoginPage && <Navbar
-            toggleSidebar={this.toggleMobileSidebar}
-            user={this.state.user}
-            toggleSidebarCollapse={this.toggleSidebarCollapse}
-          />}
+          {!isLoginPage && (
+            <Navbar
+              toggleSidebar={this.toggleMobileSidebar}
+              user={this.state.user}
+              toggleSidebarCollapse={this.toggleSidebarCollapse}
+            />
+          )}
 
           {/* Page content */}
-          <div className={`${isLoginPage ? 'p-0' : 'p-3'} bg-gray-100`}>
+          <div className={`${isLoginPage ? "p-0" : "p-3"} `}>
             <Routes>
               <Route exact path="/" element={<Login />} />
               <Route exact path="/login" element={<Login />} />
-              <Route exact path="/account-inactive" element={<AccountInactive />} />
+              <Route
+                exact
+                path="/account-inactive"
+                element={<AccountInactive />}
+              />
               <Route exact path="/user-profile" element={<Profile />} />
               <Route exact path="/verify-otp" element={<VerifyOTP />} />
-              <Route exact path="/account-profile" element={<InstitutionData />} />
-              <Route exact path="/institution-teams" element={<InstitutionTeams />} />
-              <Route exact path="/operations-certificate" element={<OperationsCert />} />
-              <Route exact path="/letter-templates" element={<InstitutionLetter />} />
-              <Route exact path="/dashboard" element={<Dashboard/>} />
-              <Route exact path="/complete-profile" element={<CompleteProfile />} />
-              <Route exact path="/document-requests" element={<DocumentRequest/>} />
-              <Route exact path="/document-requests/:documentId" element={<DocumentDetails/>} />
-              <Route exact path="/document-types" element={<DocumentTypes/>} />
-              <Route exact path="/document-types/add-remove" element={<AddDocumentType />} />
-              <Route exact path="/document-types/:documentId" element={<ValidationQuestions />} />
-              <Route exact path="/institution-teams/:institutionId" element={<InstitutionUsers />} />
+              <Route
+                exact
+                path="/account-profile"
+                element={<InstitutionData />}
+              />
+              <Route
+                exact
+                path="/institution-teams"
+                element={<InstitutionTeams />}
+              />
+              <Route
+                exact
+                path="/operations-certificate"
+                element={<OperationsCert />}
+              />
+              <Route
+                exact
+                path="/letter-templates"
+                element={<InstitutionLetter />}
+              />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route
+                exact
+                path="/complete-profile"
+                element={<CompleteProfile />}
+              />
+              <Route
+                exact
+                path="/document-requests"
+                element={<DocumentRequest />}
+              />
+              <Route
+                exact
+                path="/document-requests/:documentId"
+                element={<DocumentDetails />}
+              />
+              <Route exact path="/document-types" element={<DocumentTypes />} />
+              <Route
+                exact
+                path="/document-types/add-remove"
+                element={<AddDocumentType />}
+              />
+              <Route
+                exact
+                path="/document-types/:documentId"
+                element={<ValidationQuestions />}
+              />
+              <Route
+                exact
+                path="/institution-teams/:institutionId"
+                element={<InstitutionUsers />}
+              />
             </Routes>
           </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaSignOutAlt, FaUser } from "react-icons/fa";
-import withRouter from '../components/withRouter'
+import withRouter from "../components/withRouter";
 import Logout from "../pages/auth/Logout";
 import axios from "../axiosConfig";
 import toast from "react-hot-toast";
 import { NavLink } from "react-router-dom";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class Navbar extends React.Component {
       isLogoutModalOpen: false,
       userData: [],
       showMenu: false,
-      institutionStatus: null
+      institutionStatus: null,
     };
     this.menuRef = React.createRef();
   }
@@ -41,7 +42,7 @@ class Navbar extends React.Component {
 
       if (institutionData) {
         this.setState({
-          userData: institutionData
+          userData: institutionData,
         });
       }
     } catch (error) {
@@ -55,37 +56,40 @@ class Navbar extends React.Component {
       const institutionData = response.data.institutionData;
 
       const { status } = institutionData;
-        this.setState({
-          institutionStatus: status !== 'inactive'
-        });
+      this.setState({
+        institutionStatus: status !== "inactive",
+      });
     } catch (error) {
       toast.error(error.response.data.message);
     }
   };
 
   componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener("mousedown", this.handleClickOutside);
   }
 
   componentDidMount() {
     this.fetchUserData();
-    document.addEventListener('mousedown', this.handleClickOutside);
+    document.addEventListener("mousedown", this.handleClickOutside);
   }
 
   closeLogoutModal = () => {
     this.setState({ isLogoutModalOpen: false });
   };
   handleMenuItemClick = () => {
-      // Close the menu after clicking an item
-      this.setState({ showMenu: false });
-    };
+    // Close the menu after clicking an item
+    this.setState({ showMenu: false });
+  };
 
   render() {
     const { toggleSidebar, toggleSidebarCollapse, user } = this.props;
-    const {userData, showMenu} = this.state;
+    const { userData, showMenu } = this.state;
 
     return (
-      <div className="text-gray-800 bg-white px-4 py-[3px] flex justify-between items-center border-b font-figtree" onClick={this.handleLogoutClick}>
+      <div
+        className="text-gray-800 bg-white dark:bg-slate-900 px-4 py-[3px] flex justify-between items-center border-b font-figtree"
+        onClick={this.handleLogoutClick}
+      >
         <div className="flex items-center">
           {/* Sidebar Toggle Button (Mobile) */}
           <button
@@ -93,12 +97,12 @@ class Navbar extends React.Component {
             onClick={toggleSidebar}
           >
             <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-5"
             >
-                <path d="M17 4H3V6H17V4ZM13 11H3V13H13V11ZM17 18H3V20H17V18ZM22.0104 8.81412L20.5962 7.3999L16 11.9961L20.5962 16.5923L22.0104 15.1781L18.8284 11.9961L22.0104 8.81412Z"></path>
+              <path d="M17 4H3V6H17V4ZM13 11H3V13H13V11ZM17 18H3V20H17V18ZM22.0104 8.81412L20.5962 7.3999L16 11.9961L20.5962 16.5923L22.0104 15.1781L18.8284 11.9961L22.0104 8.81412Z"></path>
             </svg>
           </button>
 
@@ -108,62 +112,74 @@ class Navbar extends React.Component {
             onClick={toggleSidebarCollapse}
           >
             <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-5"
             >
-                <path d="M17 4H3V6H17V4ZM13 11H3V13H13V11ZM17 18H3V20H17V18ZM22.0104 8.81412L20.5962 7.3999L16 11.9961L20.5962 16.5923L22.0104 15.1781L18.8284 11.9961L22.0104 8.81412Z"></path>
+              <path d="M17 4H3V6H17V4ZM13 11H3V13H13V11ZM17 18H3V20H17V18ZM22.0104 8.81412L20.5962 7.3999L16 11.9961L20.5962 16.5923L22.0104 15.1781L18.8284 11.9961L22.0104 8.81412Z"></path>
             </svg>
-            
           </button>
 
           <h1 className="text-xl font-semibold ml-4">Dashboard</h1>
         </div>
-        
-        {/* <Logout /> */}
-        <div className="flex items-center gap-2 mr-2 hover:bg-blue-100 rounded-md px-2 py-1 cursor-pointer" onClick={this.toggleMenu}>
-          <div className="flex items-center justify-center text-gray-400 w-10 h-10 rounded-full bg-blue-50">
-            <FaUser size={24}/>
-          </div>
-          <div className="flex-1 text-sm">
-            <p className="font-semibold">{userData.first_name} {userData.other_name} {userData.last_name}</p>
-            <p className="text-xs text-gray-400 font-medium">{userData.email}</p>
-          </div>
-        </div>
 
-        {showMenu && (
+        <div className="flex gap-5 items-center">
+          <ThemeSwitcher />
+          {/* <Logout /> */}
           <div
-            ref={this.menuRef}
-            className="absolute right-4 top-10 mt-2 w-64 bg-white rounded-md shadow-lg py-2 border z-40"
+            className="flex items-center gap-2 mr-2 hover:bg-blue-100 rounded-md px-2 py-1 cursor-pointer"
+            onClick={this.toggleMenu}
           >
-            <div className="pl-2 pb-1 border-b">
-              <p className="text-sm font-semibold">Signed in as</p>
-              <p className="text-xs font-medium text-gray-400">{userData.email}</p>
+            <div className="flex items-center justify-center text-gray-400 w-10 h-10 rounded-full bg-blue-50">
+              <FaUser size={24} />
             </div>
-            <ul className="text-sm">
-              {this.state.institutionStatus ? (
-                <NavLink to={`/user-profile`}
-                  className="flex items-center gap-4 px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                  onClick={this.handleMenuItemClick}
-                >
-                  <FaUser size={16} />
-                  <span>My Profile</span>
-                </NavLink>
-              ):(
-                <NavLink
-                  className="flex items-center gap-4 px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                  onClick={this.handleMenuItemClick}
-                >
-                  <FaUser size={16} />
-                  <span>My Profile</span>
-                </NavLink>
-              )}
-              
-              <Logout />
-            </ul>
+            <div className="flex-1 text-sm">
+              <p className="font-semibold">
+                {userData.first_name} {userData.other_name} {userData.last_name}
+              </p>
+              <p className="text-xs text-gray-400 font-medium">
+                {userData.email}
+              </p>
+            </div>
           </div>
-        )}
+
+          {showMenu && (
+            <div
+              ref={this.menuRef}
+              className="absolute right-4 top-10 mt-2 w-64 bg-white rounded-md shadow-lg py-2 border z-40"
+            >
+              <div className="pl-2 pb-1 border-b">
+                <p className="text-sm font-semibold">Signed in as</p>
+                <p className="text-xs font-medium text-gray-400">
+                  {userData.email}
+                </p>
+              </div>
+              <ul className="text-sm">
+                {this.state.institutionStatus ? (
+                  <NavLink
+                    to={`/user-profile`}
+                    className="flex items-center gap-4 px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                    onClick={this.handleMenuItemClick}
+                  >
+                    <FaUser size={16} />
+                    <span>My Profile</span>
+                  </NavLink>
+                ) : (
+                  <NavLink
+                    className="flex items-center gap-4 px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                    onClick={this.handleMenuItemClick}
+                  >
+                    <FaUser size={16} />
+                    <span>My Profile</span>
+                  </NavLink>
+                )}
+
+                <Logout />
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
