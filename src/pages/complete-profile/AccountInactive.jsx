c64961815e9587fd7180db39520e446799af2175
Ebenezer Owusu <oebenezer229@gmail.com>
2024-10-21 23:14:27 +0000

    account_setup

--- src/pages/complete-profile/AccountInactive.jsx
+++ src/pages/complete-profile/AccountInactive.jsx
@@ -170,7 +170,7 @@ const AccountInactive = () => {
 
             <div className="my-4 flex items-center justify-center">
               <NavLink
-                to={`/account-setup/profile`}
+                to={`/account-setup`}
                 className="flex items-center w-44 space-x-2 bg-[#ff0404] hover:bg-[#f84848] text-white px-4 py-1.5 rounded-md"
               >
                 <FaUser /> <p>Setup Account</p>
