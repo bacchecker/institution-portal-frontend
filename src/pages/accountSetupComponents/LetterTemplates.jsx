6da09666c8288d6bbd11d23975ac8188ce252b14
Ebenezer Owusu <oebenezer229@gmail.com>
2024-10-23 20:08:53 +0000

    fix

--- src/pages/accountSetupComponents/LetterTemplates.jsx
+++ src/pages/accountSetupComponents/LetterTemplates.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { Spinner } from "@nextui-org/react";
+import React, { useState } from "react";
+import { FaAnglesLeft, FaAnglesRight } from "react-icons/fa6";
+import secureLocalStorage from "react-secure-storage";
 
-function LetterTemplates() {
+function LetterTemplates({ setActiveStep }) {
+  const [isSaving, setSaving] = useState(false);
+  const institution = secureLocalStorage.getItem("institution");
+  const setInstitution = (newInstitution) => {
+    secureLocalStorage.setItem("institution", newInstitution);
+  };
+
+  const handleBackButton = () => {
+    const updatedInstitution = {
+      ...institution,
+      current_step: "2",
+    };
+
+    setInstitution(updatedInstitution);
+    secureLocalStorage.setItem("institution", updatedInstitution);
+    setActiveStep(2);
+  };
+
+  const handleSubmit = () => {};
   return (
-    <div>LetterTemplates</div>
-  )
+    <div className="w-full px-5 pb-4">
+      <div></div>
+      <div className="flex justify-between w-full mt-[4rem!important]">
+        <button
+          type="button"
+          onClick={handleBackButton}
+          className="flex items-center bg-[#ffffff] border border-[#ff0404] hover:bg-[#ff0404] text-[#ff0404] hover:text-white px-4 py-2.5 rounded-[0.3rem] font-medium"
+        >
+          <FaAnglesLeft className="mr-2" />
+          Back
+        </button>
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className={`flex items-center bg-[#ff0404] hover:bg-[#f77f7f] text-white px-4 py-2.5 rounded-[0.3rem] font-medium ${
+            isSaving && "cursor-not-allowed bg-[#f77f7f]"
+          }`}
+          disabled={isSaving}
+        >
+          {isSaving ? (
+            <>
+              <Spinner size="sm" color="white" />
+              <span className="ml-2">Saving...</span>
+            </>
+          ) : (
+            <>
+              Save and Continue
+              <FaAnglesRight className="ml-2" />
+            </>
+          )}
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default LetterTemplates
+export default LetterTemplates;
