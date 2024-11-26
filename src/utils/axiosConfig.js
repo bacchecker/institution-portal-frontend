db5cdcd5d25db2ced061150e5ace1671b749c272
Script Kiddie <nyarkohfredrick524@gmail.com>
2024-11-26 09:57:45 +0000

    axios

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import secureLocalStorage from "react-secure-storage";
 
 const instance = axios.create({
-  //baseURL: "https://backend.baccheck.online/api/",
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://backend.baccheck.online/api/",
+  //baseURL: "http://localhost:8000/api/",
   headers: {
     // "Content-Type": "multipart/form-data",
     Accept: "application/json",
