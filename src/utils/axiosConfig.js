848061235cfce12e475fe4dd690b255aa43405b9
Script Kiddie <nyarkohfredrick524@gmail.com>
2024-12-11 12:52:26 +0000

    new base url

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import secureLocalStorage from "react-secure-storage";
 
 const instance = axios.create({
-  baseURL: "https://backend.baccheck.online/api/",
-  //baseURL: "http://aw8kkg8ck48040oc4cgo44so.67.205.158.15.sslip.io/dashboard/api/",
+  //baseURL: "https://backend.baccheck.online/api/",
+  baseURL: "http://aw8kkg8ck48040oc4cgo44so.67.205.158.15.sslip.io/dashboard/api/",
   //baseURL: "http://127.0.0.1:8000/api/",
   headers: {
     // "Content-Type": "multipart/form-data",
