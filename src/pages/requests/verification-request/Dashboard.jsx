8118cb6c310cdabb6254d0486c0cfe497407977b
Script Kiddie <nyarkohfredrick524@gmail.com>
2025-01-17 11:33:23 +0000

    month fix

--- src/pages/requests/verification-request/Dashboard.jsx
+++ src/pages/requests/verification-request/Dashboard.jsx
@@ -59,7 +59,7 @@ export default function Dashboard() {
                 <ol className="list-decimal list-inside space-y-2 text-gray-700">
                     {/* Step 1 */}
                     <li>
-                    <p className="font-medium text-black">Send Request</p>
+                    <span className="font-medium text-black">Send Request</span>
                     <ul className="list-disc list-inside pl-5 mt-1 space-y-1 text-gray-600">
                         <li>An institution initiates a verification request using E-Check.</li>
                         <li>The request includes details of the document to be verified.</li>
@@ -67,7 +67,7 @@ export default function Dashboard() {
                     </li>
                     {/* Step 2 */}
                     <li>
-                    <p className="font-medium text-black">Owner Notification</p>
+                    <span className="font-medium text-black">Document Owner Notification</span>
                     <ul className="list-disc list-inside pl-5 mt-1 space-y-1 text-gray-600">
                         <li>The document owner receives an email notification with a secure link to approve or decline the request.</li>
                         <li>The document owner has full control to approve or decline the verification request.</li>
@@ -75,7 +75,7 @@ export default function Dashboard() {
                     </li>
                     {/* Step 3 */}
                     <li>
-                    <p className="font-medium text-black">Approval or Decline</p>
+                    <span className="font-medium text-black">Approval or Decline</span>
                     <ul className="list-disc list-inside pl-5 mt-1 space-y-1 text-gray-600">
                         <li>Once approved, the requesting institution is notified, and the verification is completed by the receving institution.</li>
                     </ul>
@@ -85,72 +85,140 @@ export default function Dashboard() {
 
         </div>,
         <div className="w-full flex flex-col justify-center h-full">
-            <div className="w-full bg-black rounded-md h-48"></div>
-            <button onClick={() => {
-                    setOpenDrawer(false);
-                }} className="w-full flex justify-end mt-1 font-normal underline">Skip</button>
-            <div className="font-normal">
-                <p className="font-semibold text-black mb-3 text-lg">Features and Benefits</p>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4 p-4 text-gray-800">
-                {/* Feature 1 */}
-                <div className="flex items-start space-x-3">
-                    <div className="text-blue-500 text-xl">
-                    <i className="fas fa-check-circle"></i>
-                    </div>
-                    <div>
-                    <p className="font-semibold">Seamless Verification Process</p>
-                    <p className="text-sm">Simplified steps from request creation to document owner response.</p>
-                    </div>
-                </div>
-
-                {/* Feature 2 */}
-                <div className="flex items-start space-x-3">
-                    <div className="text-blue-500 text-xl">
-                    <i className="fas fa-envelope"></i>
-                    </div>
-                    <div>
-                    <p className="font-semibold">Secure Notifications</p>
-                    <p className="text-sm">Owners are notified via email to ensure secure approval.</p>
-                    </div>
-                </div>
-
-                {/* Feature 3 */}
-                <div className="flex items-start space-x-3">
-                    <div className="text-blue-500 text-xl">
-                    <i className="fas fa-box-open"></i>
-                    </div>
-                    <div>
-                    <p className="font-semibold">Subscription Flexibility</p>
-                    <p className="text-sm">Choose from various E-Check plans based on your institution's needs.</p>
-                    </div>
-                </div>
-
-                {/* Feature 4 */}
-                <div className="flex items-start space-x-3">
-                    <div className="text-blue-500 text-xl">
-                    <i className="fas fa-coins"></i>
-                    </div>
-                    <div>
-                    <p className="font-semibold">Efficient Credit System</p>
-                    <p className="text-sm">Credits are used for verification requests, offering transparency and control.</p>
-                    </div>
-                </div>
-
-                </div>
-
+            <div className="">
+                <p className="font-semibold mb-1 text-lg">Choose Plan</p>
+                <p className="w-full xl:w-2/3 text-xs">E-Check makes document verification fast, secure, and hassle-free. Choose a plan that fits your needs and start verifying instantly!</p>
             </div>
+            {loading ? (
+                <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 lg:gap-5 mt-4 lg:mt-8">
+                    <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+                        <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                            <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
+                                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+                            </svg>
+                        </div>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                        <div className="flex items-center mt-4">
+                        <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+                            </svg>
+                            <div>
+                                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                                <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                            </div>
+                        </div>
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+                        <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                            <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
+                                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+                            </svg>
+                        </div>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                        <div className="flex items-center mt-4">
+                        <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+                            </svg>
+                            <div>
+                                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                                <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                            </div>
+                        </div>
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+                        <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                            <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
+                                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+                            </svg>
+                        </div>
+                        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                        <div className="flex items-center mt-4">
+                        <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+                            </svg>
+                            <div>
+                                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                                <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                            </div>
+                        </div>
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                </div>
+                ):(
+                <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-5 mt-4 lg:mt-8">
+                    {plans.map((plan) => (
+                    <div
+                        key={plan.id}
+                        className="w-full rounded-xl px-3 lg:px-5 py-5 lg:py-7 bg-gray-100"
+                    >
+                        <div className="w-full flex flex-col text-[13px] pb-3 border-b mb-2">
+                            <p className="text-center">{plan?.name}</p>
+                            <p className="font-semibold text-base text-center">GH₵ {plan?.amount}</p>
+                            <p className="font-light text-center -mt-1">Non-expiry</p>
+                        </div>
+                        <div className="my-2">
+                            <p className="font-semibold text-xs">Description</p>
+                            <p className="text-gray-500 text-xs text-justify mt-2">
+                            {plan?.description}
+                            </p>
+                        </div>
+                        
+                        <div className="flex flex-col space-y-1 mt-4 text-xs">
+                            <p className="font-semibold">Features</p>
+                            <div className="flex space-x-2 text-xs">
+                                {/* <IoIosStar className="text-yellow-500" /> */}
+                                <p>{plan?.credit} Credits</p>
+                            </div>
+                            <div className="flex space-x-2 text-xs">
+                                {/* <IoIosStar className="text-yellow-500" /> */}
+                                <p>{plan?.bonus} Bonus</p>
+                            </div>
+                            <div className="flex space-x-2 text-xs">
+                                {/* <IoIosStar className="text-yellow-500" /> */}
+                                <p>{plan?.total_credit} Total Credits</p>
+                            </div>
+                        </div>
+                        
+                        <button 
+                        onClick={() => {
+                            setData(plan);
+                            setOpenDrawer(true);
+                        }}
+                        className="w-full hover:text-white text-gray-500 hover:bg-gray-500 bg-gray-300 rounded-md mt-6 py-2 text-xs"
+                        >
+                        Choose Plan
+                        </button>
+                    </div>
+                    ))}
+                    
+                </div>
+            )}
         </div>,
         
     ];
   
     const handlePrevious = () => {
-      if (currentPage > 0) {
+      if (currentPage-1 > 0) {
         setCurrentPage((prev) => prev - 1);
       }
     };
   
     const handleNext = () => {
-      if (currentPage < pages.length - 1) {
+      if (currentPage-1 < pages.length - 1) {
         setCurrentPage((prev) => prev + 1);
       }
     };
@@ -452,7 +520,7 @@ export default function Dashboard() {
                 <div className="w-full mx-auto p-2 rounded-md flex-1 overflow-y-auto">
                     <div className="w-full rounded-lg overflow-hidden flex justify-center items-center">
                         {/* Content */}
-                        <div className="w-full font-semibold text-gray-700">{pages[currentPage]}</div>
+                        <div className="w-full font-semibold text-gray-700">{pages[currentPage-1]}</div>
                     </div>
                 </div>
 
@@ -460,7 +528,7 @@ export default function Dashboard() {
                 <div className="w-full absolute bottom-0 left-0 bg-white py-4">
                     <div className="flex justify-between items-center px-4">
                         {/* Previous Button */}
-                        {currentPage > 0 && (
+                        {currentPage-1 > 0 && (
                             <button
                                 className="bg-bChkRed text-white px-6 py-1 rounded-sm text-xs hover:bg-red-500"
                                 onClick={handlePrevious}
@@ -475,14 +543,14 @@ export default function Dashboard() {
                                 <div
                                     key={index}
                                     className={`w-8 h-1 transition-all rounded-xl ${
-                                        currentPage === index ? "bg-bChkRed" : "bg-gray-200"
+                                        currentPage-1 === index ? "bg-bChkRed" : "bg-gray-200"
                                     }`}
                                 ></div>
                             ))}
                         </div>
 
                         {/* Next Button */}
-                        {currentPage < pages.length - 1 && (
+                        {currentPage-1 < pages.length - 1 && (
                             <button
                                 className="bg-bChkRed text-white px-6 py-1 rounded-sm text-xs hover:bg-red-500"
                                 onClick={handleNext}
@@ -491,7 +559,7 @@ export default function Dashboard() {
                             </button>
                         )}
                         {/* Close Button */}
-                        {currentPage === pages.length - 1 && (
+                        {currentPage-1 === pages.length - 1 && (
                             <button
                                 className="bg-bChkRed text-white px-6 py-1 rounded-sm text-xs hover:bg-red-500"
                                 onClick={() => {
