1d00d9cca085b2d6983e1a34b9a2931402e2b7fe
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2025-01-27 19:30:27 +0000

    a

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -4,8 +4,8 @@ import secureLocalStorage from "react-secure-storage";
 axios.defaults.withCredentials = true;
 
 const instance = axios.create({
-  baseURL: "https://admin-dev.baccheck.online/api/",
-  // baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "https://admin-dev.baccheck.online/api/",
+  baseURL: "http://127.0.0.1:8000/api/",
   headers: {
     "Content-Type": "multipart/form-data",
     Accept: "application/json",
