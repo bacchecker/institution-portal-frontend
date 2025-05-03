830eab6823b9b2baeacfd975fcc5956f103fa3f0
Script Kiddie <nyarkohfredrick524@gmail.com>
2025-05-03 23:43:14 +0000

    validation new logic

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -4,6 +4,7 @@ import secureLocalStorage from "react-secure-storage";
 axios.defaults.withCredentials = true;
 
 const instance = axios.create({
+  //baseURL: "http://127.0.0.1:8000/api",
   baseURL: import.meta.env.VITE_BACCHECKER_API_URL || "http://127.0.0.1:8000/api",
   headers: {
     "Content-Type": "multipart/form-data",
