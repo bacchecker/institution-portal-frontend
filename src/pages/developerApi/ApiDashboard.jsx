fa3a58cfab30c71f93826152495675859ac22d3c
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2025-07-08 07:35:11 +0000

    a

--- src/pages/developerApi/ApiDashboard.jsx
+++ src/pages/developerApi/ApiDashboard.jsx
@@ -34,6 +34,7 @@ import EditApi from "./EditApi";
 import { FaPlusCircle } from "react-icons/fa";
 import { IoTimerOutline, IoWarningOutline } from "react-icons/io5";
 import { FaRegCircleCheck } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const ApiDashboard = () => {
   const [selectedRange, setSelectedRange] = useState("7");
@@ -64,6 +65,7 @@ const ApiDashboard = () => {
   const [submittedFilters, setSubmittedFilters] = useState({});
   const [selectedScopes, setSelectedScopes] = useState([]);
   const [scrollBehavior] = React.useState("inside");
+  const navigate = useNavigate();
 
   const handleRangeChange = (e) => {
     setSelectedRange(e.target.value);
@@ -133,7 +135,6 @@ const ApiDashboard = () => {
     fetchScopes();
     fetchStatistics();
   }, [submittedFilters, currentPage, sortBy, sortOrder, selectedRange]);
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -272,23 +273,30 @@ const ApiDashboard = () => {
           <div className="w-full bg-[#f8f8f8] md:p-[0.2vw] p-[1vw] md:rounded-[0.4vw] rounded-[1.1vw] border border-[#0000000f]">
             <div className="w-full bg-[#ffffff] border border-[#0000000f] md:rounded-[0.3vw] rounded-[1vw] flex md:p-[0.5vw] p-[2vw] items-center md:gap-[0.5vw] gap-[1vw]">
               <div className="md:w-[3vw] md:h-[3vw] w-[10vw] h-[10vw] bg-[#50199d] md:rounded-[0.2vw] rounded-[0.8vw] flex items-center justify-center">
-                <MdBarChart size={20} className="text-white"/>
+                <MdBarChart size={20} className="text-white" />
               </div>
               <div className="flex flex-col">
-                <h4 className="md:text-[1vw] text-[3vw] font-[600]">Total Requests</h4>
+                <h4 className="md:text-[1vw] text-[3vw] font-[600]">
+                  Total Requests
+                </h4>
                 <h4 className="md:text-[0.8vw] text-[3.5vw] text-gray-500">
                   Last {selectedRange} Days
                 </h4>
               </div>
             </div>
             <div className="flex justify-between items-center px-2">
-              <span className="text-lg font-semibold text-gray-800">{apiStats?.total_requests} <span className="md:text-[0.9vw] text-[3.5vw] text-gray-500">Requests</span></span>
+              <span className="text-lg font-semibold text-gray-800">
+                {apiStats?.total_requests}{" "}
+                <span className="md:text-[0.9vw] text-[3.5vw] text-gray-500">
+                  Requests
+                </span>
+              </span>
             </div>
           </div>
           <div className="w-full bg-[#f8f8f8] md:p-[0.2vw] p-[1vw] md:rounded-[0.4vw] rounded-[1.1vw] border border-[#0000000f]">
             <div className="w-full bg-[#ffffff] border border-[#0000000f] md:rounded-[0.3vw] rounded-[1vw] flex md:p-[0.5vw] p-[2vw] items-center md:gap-[0.5vw] gap-[1vw]">
               <div className="md:w-[3vw] md:h-[3vw] w-[10vw] h-[10vw] bg-[#1ec43c] md:rounded-[0.2vw] rounded-[0.8vw] flex items-center justify-center">
-                <FaRegCircleCheck size={20} className="text-white"/>
+                <FaRegCircleCheck size={20} className="text-white" />
               </div>
               <div className="flex flex-col">
                 <h4 className="md:text-[1vw] text-[3vw] font-[600]">
@@ -302,21 +310,25 @@ const ApiDashboard = () => {
             <div className="flex justify-between items-center px-2">
               <span className="text-lg font-semibold text-gray-800">
                 {apiStats?.total_requests > 0
-                ? Number(
-                    (apiStats.successful_requests / apiStats.total_requests) * 100
-                  ).toFixed(2)
-                : "0.00"}%
+                  ? Number(
+                      (apiStats.successful_requests / apiStats.total_requests) *
+                        100
+                    ).toFixed(2)
+                  : "0.00"}
+                %
               </span>
             </div>
           </div>
-          
+
           <div className="w-full bg-[#f8f8f8] md:p-[0.2vw] p-[1vw] md:rounded-[0.4vw] rounded-[1.1vw] border border-[#0000000f]">
             <div className="w-full bg-[#ffffff] border border-[#0000000f] md:rounded-[0.3vw] rounded-[1vw] flex md:p-[0.5vw] p-[2vw] items-center md:gap-[0.5vw] gap-[1vw]">
               <div className="md:w-[3vw] md:h-[3vw] w-[10vw] h-[10vw] bg-[#ff0404] md:rounded-[0.2vw] rounded-[0.8vw] flex items-center justify-center">
-                <IoWarningOutline size={20} className="text-white"/>
+                <IoWarningOutline size={20} className="text-white" />
               </div>
               <div className="flex flex-col">
-                <h4 className="md:text-[1vw] text-[3vw] font-[600]">Error Rate</h4>
+                <h4 className="md:text-[1vw] text-[3vw] font-[600]">
+                  Error Rate
+                </h4>
                 <h4 className={`md:text-[0.8vw] text-[3.5vw] text-gray-500`}>
                   {apiStats?.failed_requests} Failed
                 </h4>
@@ -325,27 +337,35 @@ const ApiDashboard = () => {
             <div className="flex justify-between items-center px-2">
               <span className="text-lg font-semibold text-gray-800">
                 {apiStats?.total_requests > 0
-                ? Number(
-                    (apiStats.failed_requests / apiStats.total_requests) * 100
-                  ).toFixed(2)
-                : "0.00"}%
+                  ? Number(
+                      (apiStats.failed_requests / apiStats.total_requests) * 100
+                    ).toFixed(2)
+                  : "0.00"}
+                %
               </span>
             </div>
           </div>
           <div className="w-full bg-[#f8f8f8] md:p-[0.2vw] p-[1vw] md:rounded-[0.4vw] rounded-[1.1vw] border border-[#0000000f]">
             <div className="w-full bg-[#ffffff] border border-[#0000000f] md:rounded-[0.3vw] rounded-[1vw] flex md:p-[0.5vw] p-[2vw] items-center md:gap-[0.5vw] gap-[1vw]">
               <div className="md:w-[3vw] md:h-[3vw] w-[10vw] h-[10vw] bg-[#818712] md:rounded-[0.2vw] rounded-[0.8vw] flex items-center justify-center">
-                <IoTimerOutline size={20} className="text-white"/>
+                <IoTimerOutline size={20} className="text-white" />
               </div>
               <div className="flex flex-col">
-                <h4 className="md:text-[1vw] text-[3vw] font-[600]">Avg Response Time</h4>
+                <h4 className="md:text-[1vw] text-[3vw] font-[600]">
+                  Avg Response Time
+                </h4>
                 <h4 className={`md:text-[0.8vw] text-[3.5vw] text-gray-500`}>
                   Average response time
                 </h4>
               </div>
             </div>
             <div className="flex justify-between items-center px-2">
-              <span className="text-lg font-semibold text-gray-800">{Number(apiStats?.avg_response_time || 0).toFixed(2)}<span className="md:text-[0.9vw] text-[3.5vw] text-gray-500">ms</span></span>
+              <span className="text-lg font-semibold text-gray-800">
+                {Number(apiStats?.avg_response_time || 0).toFixed(2)}
+                <span className="md:text-[0.9vw] text-[3.5vw] text-gray-500">
+                  ms
+                </span>
+              </span>
             </div>
           </div>
         </div>
@@ -421,7 +441,10 @@ const ApiDashboard = () => {
                       <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 md:gap-4 xl:gap-6">
                         {Object.entries(apiScopes || {}).map(
                           ([resource, scopes]) => (
-                            <div key={resource} className="h-full bg-white border border-gray-200 rounded-md p-[1vw] flex flex-col justify-between">
+                            <div
+                              key={resource}
+                              className="h-full bg-white border border-gray-200 rounded-md p-[1vw] flex flex-col justify-between"
+                            >
                               <div className="flex items-center gap-[0.5vw]">
                                 <h2 className="text-[14px] capitalize font-[600]">
                                   {`Manage ${resource.replace("-", " ")}`}
@@ -471,7 +494,9 @@ const ApiDashboard = () => {
                                         }}
                                       />
                                       <span>
-                                        <p className="text-[13px]">{scope.name}</p>
+                                        <p className="text-[13px]">
+                                          {scope.name}
+                                        </p>
                                       </span>
                                     </label>
                                   </div>
@@ -501,7 +526,7 @@ const ApiDashboard = () => {
             )}
           </ModalContent>
         </Modal>
-        
+
         <section className="mb-4">
           <div className="relative md:w-full flex bg-[#f8f8f8] justify-between items-center w-full mx-auto rounded-none shadow-none border-none p-4">
             <div className="w-full flex items-center justify-between">
@@ -593,7 +618,9 @@ const ApiDashboard = () => {
             {apiKeys?.data?.map((item) => (
               <TableRow
                 key={item?.id}
-                className={`border-b ${item?.status == "revoked" ? "text-red-400 bg-red-100" : ""}`}
+                className={`border-b ${
+                  item?.status == "revoked" ? "text-red-400 bg-red-100" : ""
+                }`}
               >
                 <TableCell className="font-semibold">{item?.name}</TableCell>
                 <TableCell>
@@ -663,9 +690,9 @@ const ApiDashboard = () => {
                             <p>Show Secret</p>
                           </button>
                           <button
-                            onClick={() => {
-                              window.location.href = `/developers-api/${item.id}`;
-                            }}
+                            onClick={() =>
+                              navigate(`/developers-api/${item.id}`)
+                            }
                             className="text-left text-sm hover:bg-bChkRed hover:text-white px-4 py-1.5 rounded-md w-full flex space-x-2 items-center text-gray-700"
                           >
                             <FaEye size={17} />
@@ -700,7 +727,6 @@ const ApiDashboard = () => {
         )}
       </div>
     </>
-    
   );
 };
 
